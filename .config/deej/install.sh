2b9339fac4a2cffd2dc0261039aa62450056d7e8
Omar <omarnelson1533@gmail.com>
2021-03-26 20:44:33 -0600

    Added deej configuration directory
    Created the service file required to auto-start deej, should create a
    special script to create the service file that runs with root privileges

---
+++ .config/deej/install.sh
@@ -0,0 +1,6 @@
+# This configuration file should be installed on /etc/systemd/user/
+# Only root is allowed to write to this directory
+# After installation, the following commands need to be run from the user
+# systemctl --user daemon-reload
+# systemctl --user enable deej.service
+# systemctl --user start deej.service
